cb2835cb8be26a2aa55b622eea42fa0ad67ea3e8
Kari <kari.lynn.dreyer@gmail.com>
2024-11-21 21:28:21 +0100

    Develop main component to render the app UI

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import React Native components
+import { StyleSheet } from 'react-native';
 
-export default function App() {
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// import the screens
+import Start from './components/Start.js';
+import Chat from './components/Chat.js';
+
+// Create the navigator
+const Stack = createNativeStackNavigator();
+
+// The app’s main component that renders the chat UI
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Start"
+        >
+          <Stack.Screen
+            name="Start"
+            component={Start}
+          />
+          <Stack.Screen
+            name="Chat"
+            component={Chat}
+          />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,3 +40,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
