c0b9459889ecc533dcbbc801e175a48293ba4842
Kari <kari.lynn.dreyer@gmail.com>
2024-12-16 13:12:49 +0100

    Feat: Allow users to send images

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,11 +1,11 @@
-// Import necessary libraries and dependencies
 import { Text, TouchableOpacity, StyleSheet, View, Alert } from "react-native";
 import { useActionSheet } from '@expo/react-native-action-sheet';
 import * as ImagePicker from 'expo-image-picker';
 import * as Location from 'expo-location';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
-// Create a function to handle the custom actions options
-const CustomActions = ({ wrapperStyle, iconTextStyle, uid, onSend }) => {
+
+const CustomActions = ({ iconTextStyle, name, onSend, storage, uid, wrapperStyle }) => {
     const actionSheet = useActionSheet();
     
     //display a menu with options (take photo, select photo, share location)
@@ -32,65 +32,66 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, uid, onSend }) => {
             },
         );
     };
+    
+    // Function to upload and send an image
+    const uploadAndSendImage = async (imageURI) => {
+        const uniqueRefString = generateReference(imageURI);
+        const response = await fetch(imageURI);
+        const blob = await response.blob();
+        const newUploadRef = ref(storage, uniqueRefString);
+        uploadBytes(newUploadRef, blob).then(async (snapshot) => {
+            console.log('File has been uploaded successfully');
+            const imageURL = await getDownloadURL(snapshot.ref);
+            const imageMessage = {
+                user: { _id: uid, name },
+                createdAt: new Date(),
+                image: imageURL
+            };
+            onSend([imageMessage]);
+          })
+    }
+
+    // Generate a reference for the image
+    const generateReference = (uri) => {
+        const timeStamp = (new Date()).getTime();
+        const imageName = uri.split("/")[uri.split("/").length - 1];
+        return `${uid}-${timeStamp}-${imageName}`;
+      }
 
     // Allow user to pick a photo from their library
     const pickImage = async () => {
         let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync(); // Ask for permission to access the user's media library
-  
         if (permissions?.granted) {
             let result = await ImagePicker.launchImageLibraryAsync();
-  
-            if (!result.canceled) {
-                console.log('uploading and uploading the image occurs here');
-              } else Alert.alert("Permission hasn't been granted.");
+            if (result?.assets?.length > 0) {
+                await uploadAndSendImage(result.assets[0].uri);
+            }
+        } else {
+            Alert.alert("Permission hasn't been granted.");
         }
     };
 
     // Allow user to take a photo with their camera
     const takePhoto = async () => {
         let permissions = await ImagePicker.requestCameraPermissionsAsync(); // Ask for permission to access the user's camera
-    
         if (permissions?.granted) {
             let result = await ImagePicker.launchCameraAsync();
-    
-            if (!result.canceled) {
-                console.log('uploading and uploading the image occurs here');
-              } else Alert.alert("Permission hasn't been granted.");
+            if (result?.assets?.length > 0) {
+                await uploadAndSendImage(result.assets[0].uri);
+            }
+        } else {
+            Alert.alert("Permission hasn't been granted.");
         }
     };
 
     // Allow user to share their location
-    // const getLocation = async () => {
-    //     let permissions = await Location.requestForegroundPermissionsAsync(); // Ask for permission to access the user's location
-    //     if (permissions?.granted) {
-    //         const location = await Location.getCurrentPositionAsync({});
-    //         if (location) {
-    //             onSend({
-    //                 user: {
-    //                     _id: uid,
-    //                 },
-    //                 createdAt: new Date(),
-    //                 location: {
-    //                     longitude: location.coords.longitude,
-    //                     latitude: location.coords.latitude,
-    //                 },
-    //             });
-    //         } else Alert.alert("Error occurred while fetching location");
-    //     } else Alert.alert("Permission hasn't been granted.");
-    // };
-
     const getLocation = async () => {
-        console.log("getLocation triggered"); // Debugging
         try {
             const { status } = await Location.requestForegroundPermissionsAsync();
-            console.log("Permission status:", status); // Log permission status
             if (status === 'granted') {
                 const location = await Location.getCurrentPositionAsync({});
-                console.log("Location fetched:", location); // Log the location data
                 const locationMessage = {
-                    user: {
-                        _id: uid,
-                    },
+                    user: { _id: uid, name },
                     createdAt: new Date(),
                     location: {
                         latitude: location.coords.latitude,
@@ -98,9 +99,8 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, uid, onSend }) => {
                     },
                 };
                 onSend([locationMessage]); // Send the location as the message
-                console.log("Location message sent:", locationMessage);
             } else {
-                console.log("Permission denied");
+                Alert.alert("Permission hasn't been granted.");
             }
         } catch (error) {
             console.error("Error fetching location:", error);
